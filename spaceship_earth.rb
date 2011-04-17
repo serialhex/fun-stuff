006e461b01eb37bdf805cd0715dfc252213c8f82
Justin Patera <serialhex@gmail.com>
2011-04-17 01:30:09 -0400

    edited the doc on spaceship earth to be a bit more descriptive, nothing to see here, move along people...

--- spaceship_earth.rb
+++ spaceship_earth.rb
@@ -1,6 +1,17 @@
 #!/usr/bin/env ruby
 
 # spiffy program to find out how far you travel in a given amount of time
+# until i create an input parser, import the SOB into irb & work on it in there
+# or dont, i'm just saying...
+#
+# btw, this is not incredibly accurate as we're traveling at a little over 29km/sec
+# through space, and i haven't taken into account things like a stellar day
+# (the length of a day with the stars being in the same place is  ~4 mins shy of a 24hr day)
+# ones altitude, the eccentricity of earths orbit around the sun, it's axial tilt, and other
+# such factors, so i highly recommend *AGAINST* using this for precice measurements, i.e.
+# astrology, the predicting of omens, finding your "one true love", or finding the safest place
+# to be when quetzacoatyl comes back (btw, that's Time.new(2012, 12, 22) if you want to figure out
+# how far you are going to travel before the world ends...)
 
 require 'mathn'
 
